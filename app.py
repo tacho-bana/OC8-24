8f0abe724550d390d848d888ec5105eb0480a5da
s2322087 <s2322087@stu.musashino-u.ac.jp>
2024-07-12 14:56:23 +0900

    一応完成

--- app.py
+++ app.py
@@ -11,9 +11,14 @@ def analyze_audio(file_path):
     return y, sr, rms_normalized
 
 def get_background_color(intensity):
-    red = int(255 * intensity)
-    green = int(255 * (1 - intensity))
-    return (red, green, 0)
+    if intensity < 0.1:
+        # No sound: Blue color
+        return (0, 0, 255)
+    else:
+        # Interpolated color between green and red based on intensity
+        red = int(255 * (intensity - 0.1) / 0.9)
+        green = int(255 * (1 - (intensity - 0.1) / 0.9))
+        return (red, green, 0)
 
 def make_frame(t, y, sr, rms_normalized, fps):
     fig, ax = plt.subplots(figsize=(10, 2))
@@ -21,20 +26,21 @@ def make_frame(t, y, sr, rms_normalized, fps):
     end = int((t + 1 / fps) * sr)
     ax.plot(y[start:end])
     ax.axis('off')
-    intensity_index = int(t * fps / len(rms_normalized) * len(rms_normalized))
+    intensity_index = int(t * len(rms_normalized) / duration)
+    intensity_index = min(intensity_index, len(rms_normalized) - 1)
     color = get_background_color(rms_normalized[intensity_index])
     fig.patch.set_facecolor(np.array(color) / 255)
     frame = mplfig_to_npimage(fig)
     plt.close(fig)
     return frame
 
-audio_path = '/Users/tachibananoyushou/OC8-24/mirai-demo-HardRock.mp3'
+audio_path = 'y2mate.com - Mrs GREEN APPLEナハトムジークOfficial Music Video.mp3'
 y, sr, rms_normalized = analyze_audio(audio_path)
 fps = 24
 duration = len(y) / sr
 
 video = VideoClip(lambda t: make_frame(t, y, sr, rms_normalized, fps), duration=duration)
-audio = AudioFileClip(audio_path)
+audio = AudioFileClip(audio_path).subclip(0, duration)
 video = video.set_audio(audio)
 output_path = 'output_video.mp4'
 video.write_videofile(output_path, fps=fps)
